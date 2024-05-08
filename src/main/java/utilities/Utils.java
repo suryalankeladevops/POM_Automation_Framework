e37dc4d8bfcb6cbd081351a43d8b42cef4c6783a
suryalankeladevops <138316400+suryalankeladevops@users.noreply.github.com>
2024-05-08 21:31:34 +0530

    Update Utils.java

--- src/main/java/utilities/Utils.java
+++ src/main/java/utilities/Utils.java
@@ -37,4 +37,15 @@ public class Utils {
 		return new EdgeDriver();
 
 	}
+
+	// Below code added screenshot and used in BaseTest java class
+	public static String captureScreenshot(WebDriver driver, String screenShotName) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String dest = System.getProperty("user.dir") + "/ErrorScreenshots/" + screenShotName + ".png";
+		File destination = new File(dest);
+		FileUtils.copyFile(source, destination);
+
+		return dest;
+	}
 }
